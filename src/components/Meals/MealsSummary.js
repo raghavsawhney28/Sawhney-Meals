2de9ccddd6698e269c559ddbad0ffdf66015b26b
raghavsawhney28 <raghavsawhney28@gmail.com>
2025-05-08 17:48:56 +0530

    ..

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Handpicked Meals, Crafted With Love 💖
+</h2>
+      <p>
+        Relive our special moments with every bite. This menu isn't just food — it’s a memory of us — recreated with your favorite dishes from that one perfect date.
+From cozy comfort bites to the burger you ordered with that adorable smile, everything is cooked with care, high-quality ingredients, and a whole lot of love.
+      </p>
+      <p>
+        Because you deserve to taste happiness — just like that day. 🍔🌸
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
