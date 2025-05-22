4c989471a08015a3faeead07a53b8146494b01f0
raghavsawhney28 <raghavsawhney28@gmail.com>
2025-05-22 13:28:08 +0530

    .

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,11 +3,14 @@ import { Fragment } from 'react';
 import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import khyatumeals from '../../assets/khyatumeals.jpg';
+
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
+        <img src={khyatumeals} alt='khyatu' className={classes.image}></img>
         <h1>Khyatuu Meals</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
